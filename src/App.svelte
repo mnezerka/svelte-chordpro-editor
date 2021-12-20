c3b549cf134ef61e7bcdf14c6a5a01bf3588e46a
mnezerka <michal.nezerka@sap.com>
2021-12-20 08:16:21 +0100

    implementation of debouncing - delayed readraw of preview

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,49 @@
 <script>
-	import Navbar from "./Navbar.svelte"
-	import ChrodProPreview from "./ChordProPreview.svelte"
+    import Navbar from "./Navbar.svelte"
+    import ChrodProPreview from "./ChordProPreview.svelte"
 
     export let song = "";
 
-    let source = song;
+    // this is chordpro source rendered in editor
+    let source_editor = song;
+
+    // this is chordpro source passed to preview, the reason
+    // for this duplicate of source is debouncing - delayed
+    // preview (not to redraw preview immediately after each change)
+    let source_preview = "";
+ 
+    // delay function execution to avoid too many updates
+    //  during e.g. resizing window by mouse
+    function debounce(func, delay) {
+        let timer;
+        return function () {
+            const context = this;
+            const args = arguments;
+            clearTimeout(timer);
+            timer = setTimeout(() => func.apply(context, args), delay);
+        };
+    };
+
+    const debounced_update_source = debounce(update_source, 300)
+
+    // method to be called on each change of chordpro source in editor
+    function update_source(new_source) {
+        source_preview = new_source;
+    }
+
+    // this is a way how to react on change of `source_editor`
+    $: debounced_update_source(source_editor)
+
 </script>
 
 <Navbar />
 <main>
-    <div class="editor">
-        <textarea bind:value={source} class="no-print" />
+    <div class="editor no-print">
+        <textarea bind:value={source_editor} class="no-print" />
     </div>
 
     <div class="preview">
-        <ChrodProPreview {source} />
+        <ChrodProPreview source={source_preview} />
     </div>
 </main>
 
@@ -36,9 +65,9 @@
         height: 100%;
     }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
